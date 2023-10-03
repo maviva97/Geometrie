24a03614b7d928a38eccf39dd68b5d73293bc2b2
maviva97@gmail.com <maviva97@gmail.com>
2023-10-03 13:08:29 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    float radius;
+    Float pi = 3.14F;
+     public float calculateArea() {
+        float Aria;
+        return Aria = radius*radius*pi;
+    }
+public Circle(float radius){
+this.radius=radius;
+
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle s= new Circle(5);
+        System.out.println(s.calculateArea()) ;
+    }
+}
